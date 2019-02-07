6ef09ef45f0023db3218061d0c90fb3dc266267a
L432 <l432432@gmail.com>
2019-02-07 18:01:49 +0200

    06-02-2019

--- OlegShowTypes.pas
+++ OlegShowTypes.pas
@@ -4,14 +4,9 @@ interface
 
 uses
   OlegTypePart2, StdCtrls, OlegType, IniFiles, Classes, OlegFunction;
-//
-//uses
-//  StdCtrls, IniFiles, Windows, ComCtrls, ArduinoDevice, OlegType, Series,
-//  Measurement, ExtCtrls, Classes, ArduinoDeviceShow, TeCanvas;
-//
+
 const DoubleConstantSection='DoubleConstant';
       NoFile='no file';
-//      RangeSection='Range';
       WindowCaptionFooter=' input';
       WindowTextFooter=' value is expected';
 
@@ -122,6 +117,7 @@ type
 
   TIntegerParameterShow=class (TParameterShowNew)
    private
+    fIsPositive:boolean;
     FDefaulValue:integer;
     function StringToExpectedStringConvertion(str:string):string;override;
     function GetData:integer;
@@ -130,6 +126,7 @@ type
     function ReadStringValueFromIniFile(ConfigFile:TIniFile;NameIni:string):string;override;
     procedure WriteNumberToIniFile(ConfigFile:TIniFile;NameIni:string);override;
    public
+    property IsPositive:boolean read fIsPositive write fIsPositive;
     property DefaulValue:integer read FDefaulValue write SetDefaulValue;
     Constructor Create(STD:TStaticText;
                        STC:TLabel;
@@ -168,146 +165,12 @@ type
     );overload;
     property Data:ShortInt read GetData write SetData;
   end;    //TStringParameterShow=class (TParameterShow)
-//
-//
-//TLimitShow=class(TNamedInterfacedObject)
-//private
-//  UpDownHigh,UpDownLow:TUpDown;
-//  fDivisor:byte;
-//  fDigitNumber:byte;
-//  fHookForGraphElementApdate:TSimpleEvent;
-//  procedure UpDownBegin(MaxValue: Double; UpDown: TUpDown);
-//  function GetValue(UpDown: TUpDown): double;virtual;
-//  function GetValueString(UpDown: TUpDown): string;virtual;
-//  function PositionIsEqual():boolean;
-//  procedure UpDownHighClick(Sender: TObject; Button: TUDBtnType);
-//  procedure UpDownLowClick(Sender: TObject; Button: TUDBtnType);
-//  function GetHighValue():double;virtual;
-//  function GetLowValue():double;virtual;
-//public
-//  ValueLabelHigh,ValueLabelLow:TLabel;
-//  property HookForGraphElementApdate:TSimpleEvent
-//             read fHookForGraphElementApdate write fHookForGraphElementApdate;
-//  property HighValue:double read GetHighValue;
-//  property LowValue:double read GetLowValue;
-//  Constructor Create(Nm: string;
-//                     MaxValue: Double;
-//                     DigitNumber: Byte;
-//                     UDH, UDL: TUpDown;
-//                     VLH, VLL: TLabel);overload;
-//  Constructor Create(Nm: string;
-//                     MaxValue:double;
-//                     DigitNumber:byte;
-//                     UDH,UDL:TUpDown;
-//                     VLH,VLL:TLabel;
-//                     HookFunction:TSimpleEvent);overload;
-// procedure ReadFromIniFile(ConfigFile: TIniFile);
-// procedure WriteToIniFile(ConfigFile: TIniFile);
-//end;
-//
-//TLimitShowRev=class(TLimitShow)
-//private
-//  function GetHighValue():double;override;
-//  function GetLowValue():double;override;
-//  function GetValue(UpDown: TUpDown): double;override;
-//  function GetValueString(UpDown: TUpDown): string;override;
-//public
-//end;
-//
-//TDAC_Show=class
-//  private
-//   fOutputInterface: IDAC;
-//   fValueShow:TDoubleParameterShow;
-//   fKodShow:TIntegerParameterShow;
-//   ValueSetButton,KodSetButton,ResetButton:TButton;
-//   procedure ValueSetButtonAction(Sender:TObject);
-//   procedure KodSetButtonAction(Sender:TObject);
-//   procedure ResetButtonClick(Sender:TObject);
-//  public
-//   Constructor Create(OI: IDAC;
-//                      VData,KData:TStaticText;
-//                      VL, KL: TLabel;
-//                      VSB, KSB, RB: TButton);
-//    procedure ReadFromIniFile(ConfigFile:TIniFile);virtual;
-//    procedure WriteToIniFile(ConfigFile:TIniFile);virtual;
-//    procedure Free;
-//end;
-//
-//  TArduinoDACShow=class(TArduinoSetterShow)
-//  private
-//   fDAC_Show:TDAC_Show;
-//  public
-//   Constructor Create(ArdDAC:TArduinoDAC;
-//                       PinLs: array of TPanel;
-//                       PinVariant:TStringList;
-//                       VData,KData:TStaticText;
-//                       VL, KL: TLabel;
-//                       VSB, KSB, RB: TButton);
-//   Procedure Free;
-//   procedure ReadFromIniFile(ConfigFile:TIniFile);override;
-//   Procedure WriteToIniFile(ConfigFile:TIniFile);override;
-//  end;
-//
-//function LastFileName(Mask:string):string;
-//{повертає назву (повну, з розширенням) останього файлу в
-//поточному каталозі, чия назва задовольняє маску Mask}
-//
-//function LastDATFileName():string;
-//{повертає назву (коротку) останього .dat файлу в
-//поточному каталозі}
-//
-//Procedure MelodyShot();
-//
-//Procedure MelodyLong();
-//
-//
-//
+
 implementation
 
 uses
   SysUtils, Controls, Dialogs, Graphics;
-//
-//uses
-//  Dialogs, SysUtils, Math, Controls, Graphics, Forms, OlegFunction;
-//
-//
-//function LastFileName(Mask:string):string;
-// var SR : TSearchRec;
-//     tm:integer;
-//begin
-// Result:=NoFile;
-// if FindFirst(Mask, faAnyFile, SR) <> 0 then Exit;
-// Result:=SR.name;
-// tm:=SR.time;
-// while FindNext(SR) = 0 do
-//   if tm<SR.time then
-//     begin
-//     Result:=SR.name;
-//     tm:=SR.time;
-//     end;
-// FindClose(SR);
-//end;
-//
-//function LastDATFileName():string;
-//begin
-//  Result:=LastFileName('*.dat');
-//  if Result<>NoFile then
-//   Result:=Copy(Result,1,Length(Result)-4);
-//end;
-//
-//Procedure MelodyShot();
-//begin
-//  Windows.Beep(100,50);
-//end;
-//
-//Procedure MelodyLong();
-//begin
-// Windows.Beep(700,100);
-// Windows.Beep(200,100);
-// Windows.Beep(500,100);
-//end;
-//
-//
+
 Constructor TDoubleParameterShow.Create(STD:TStaticText;
                        STC:TLabel;
                        ParametrCaption:string;
@@ -399,144 +262,7 @@ begin
 end;
 
 
-//{ LimitShow }
-//
-//constructor TLimitShow.Create(Nm: string;
-//                              MaxValue: Double;
-//                              DigitNumber: Byte;
-//                              UDH, UDL: TUpDown;
-//                              VLH, VLL: TLabel);
-//begin
-// inherited Create;
-// fDigitNumber:=DigitNumber;
-// UpDownHigh:=UDH;
-// UpDownLow:=UDL;
-// ValueLabelHigh:=VLH;
-// ValueLabelLow:=VLL;
-// fDivisor:=round(Power(10,fDigitNumber));
-// UpDownBegin(MaxValue,UpDownHigh);
-// UpDownBegin(MaxValue,UpDownLow);
-// UpDownHigh.OnClick:=UpDownHighClick;
-// UpDownLow.OnClick:=UpDownLowClick;
-// fName:=Nm;
-//end;
-//
-//constructor TLimitShow.Create(Nm: string;
-//                              MaxValue: double;
-//                              DigitNumber: byte;
-//                              UDH,UDL: TUpDown;
-//                              VLH, VLL: TLabel;
-//                              HookFunction: TSimpleEvent);
-//begin
-// Create(Nm, MaxValue, DigitNumber, UDH, UDL, VLH, VLL);
-// HookForGraphElementApdate:=HookFunction;
-//end;
-//
-//function TLimitShow.GetHighValue: double;
-//begin
-//  Result:=GetValue(UpDownHigh);
-//end;
-//
-//function TLimitShow.GetLowValue: double;
-//begin
-//  Result:=GetValue(UpDownLow);
-//end;
-//
-//function TLimitShow.GetValue(UpDown: TUpDown): double;
-//begin
-// Result:=UpDown.Position/fDivisor;
-//end;
-//
-//function TLimitShow.GetValueString(UpDown: TUpDown): string;
-//begin
-// Result:=FloatToStrF(GetValue(UpDown),ffFixed, 4, fDigitNumber);
-//end;
-//
-//function TLimitShow.PositionIsEqual: boolean;
-//begin
-// Result:=(UpDownHigh.Position-UpDownLow.Position)<1;
-//end;
-//
-//procedure TLimitShow.ReadFromIniFile(ConfigFile: TIniFile);
-//  var temp:Smallint;
-//begin
-//
-//  temp:=ConfigFile.ReadInteger(RangeSection,fName+'Max',UpDownHigh.Max);
-//  if (temp>UpDownHigh.Max)or(temp<0) then  temp:=UpDownHigh.Max;
-//  UpDownHigh.Position:=temp;
-//
-//  temp:=ConfigFile.ReadInteger(RangeSection,fName+'Min',0);
-//  if (temp>UpDownHigh.Max)or(temp<0) then  temp:=0;
-//  UpDownLow.Position:=temp;
-//
-//  if UpDownHigh.Position<=UpDownLow.Position then
-//    begin
-//      UpDownHigh.Position:=UpDownHigh.Max;
-//      UpDownLow.Position:=0;
-//    end;
-//  ValueLabelHigh.Caption:=GetValueString(UpDownHigh);
-//  ValueLabelLow.Caption:=GetValueString(UpDownLow);
-//  HookForGraphElementApdate();
-//end;
-//
-//procedure TLimitShow.UpDownBegin(MaxValue: Double; UpDown: TUpDown);
-//begin
-// UpDown.Min:=0;
-// UpDown.Max:=round(MaxValue*fDivisor);
-// UpDown.Increment:=1;
-//end;
-//
-//procedure TLimitShow.UpDownHighClick(Sender: TObject; Button: TUDBtnType);
-//begin
-//  if PositionIsEqual then
-//   begin
-//    UpDownHigh.Position:=UpDownHigh.Position+UpDownHigh.Increment;
-//    Exit;
-//   end;
-//  ValueLabelHigh.Caption:=GetValueString(UpDownHigh);
-//  HookForGraphElementApdate();
-//end;
-//
-//procedure TLimitShow.UpDownLowClick(Sender: TObject; Button: TUDBtnType);
-//begin
-//  if PositionIsEqual then
-//   begin
-//    UpDownLow.Position:=UpDownLow.Position-UpDownLow.Increment;
-//    Exit;
-//   end;
-//  ValueLabelLow.Caption:=GetValueString(UpDownLow);
-//  HookForGraphElementApdate();
-//end;
-//
-//procedure TLimitShow.WriteToIniFile(ConfigFile: TIniFile);
-//begin
-//    WriteIniDef(ConfigFile,RangeSection,fName+'Max',UpDownHigh.Position,UpDownHigh.Max);
-//    WriteIniDef(ConfigFile,RangeSection,fName+'Min',UpDownLow.Position,0);
-//end;
-//
-//{ LimitShowRev }
-//
-//function TLimitShowRev.GetHighValue: double;
-//begin
-// Result:=GetValue(UpDownLow);
-//end;
-//
-//function TLimitShowRev.GetLowValue: double;
-//begin
-// Result:=GetValue(UpDownHigh);
-//end;
-//
-//function TLimitShowRev.GetValue(UpDown: TUpDown): double;
-//begin
-//   Result:=(UpDown.Max-UpDown.Position)/fDivisor;
-//end;
-//
-//function TLimitShowRev.GetValueString(UpDown: TUpDown): string;
-//begin
-//  Result:=FloatToStrF(-GetValue(UpDown),ffFixed, 4, fDigitNumber);
-//end;
-//
-//
+
 { TIntegerParameterShow }
 
 constructor TIntegerParameterShow.Create(STD: TStaticText; STC: TLabel;
@@ -545,6 +271,7 @@ begin
   inherited Create(STD,STC,ParametrCaption,WT);
   STData.Caption:=IntToStr(InitValue);
   DefaulValue:=InitValue;
+  fIsPositive:=false;
 end;
 
 constructor TIntegerParameterShow.Create(STD: TStaticText; STC: TLabel;
@@ -555,19 +282,24 @@ end;
 
 function TIntegerParameterShow.GetData: integer;
 begin
-  Result:=StrToInt(STData.Caption);
+  if fIsPositive then Result:=abs(StrToInt(STData.Caption))
+                 else Result:=StrToInt(STData.Caption);
 end;
 
 function TIntegerParameterShow.ReadStringValueFromIniFile(ConfigFile: TIniFile;
   NameIni: string): string;
 begin
- Result:=IntToStr(ConfigFile.ReadInteger(fName,NameIni,DefaulValue));
+ if fIsPositive then
+   Result:=IntToStr(abs(ConfigFile.ReadInteger(fName,NameIni,DefaulValue)))
+                else
+   Result:=IntToStr(ConfigFile.ReadInteger(fName,NameIni,DefaulValue));
 end;
 
 procedure TIntegerParameterShow.SetData(value: integer);
 begin
   try
-    STData.Caption:=IntToStr(value);
+    if fIsPositive then  STData.Caption:=IntToStr(abs(value))
+                   else  STData.Caption:=IntToStr(value);
   finally
 
   end;
@@ -575,12 +307,14 @@ end;
 
 procedure TIntegerParameterShow.SetDefaulValue(const Value: integer);
 begin
- FDefaulValue := Value;
+ if fIsPositive then FDefaulValue := abs(Value)
+                else FDefaulValue := Value;
 end;
 
 function TIntegerParameterShow.StringToExpectedStringConvertion(str: string): string;
 begin
- Result:=IntToStr(StrToInt(str));
+ if fIsPositive then Result:=IntToStr(abs(StrToInt(str)))
+                else Result:=IntToStr(StrToInt(str));
 end;
 
 procedure TIntegerParameterShow.WriteNumberToIniFile(ConfigFile: TIniFile;
@@ -681,107 +415,7 @@ begin
   if Value then STData.Font.Color:=clPurple
            else STData.Font.Color:=clBlack;
 end;
-//
-//{ TDAC_ShowNew }
-//
-//constructor TDAC_Show.Create(OI: IDAC;
-//                                VData, KData: TStaticText;
-//                                VL, KL: TLabel;
-//                                VSB, KSB, RB: TButton);
-//begin
-//  fOutputInterface:=OI;
-//  fValueShow:=TDoubleParameterShow.Create(VData,VL,'Output value',0,5);
-//  fValueShow.ForUseInShowObject(OI);
-//
-//  ValueSetButton:=VSB;
-//  ValueSetButton.OnClick:=ValueSetButtonAction;
-//  ValueSetButton.Caption:='Set value';
-//  ResetButton:=RB;
-//  ResetButton.OnClick:=ResetButtonClick;
-//  ResetButton.Caption:='Reset';
-//
-//  fKodShow:=TIntegerParameterShow.Create(KData,KL,'Output code',0);
-//  fKodShow.ForUseInShowObject(OI);
-//
-//  KodSetButton:=KSB;
-//  KodSetButton.OnClick:=KodSetButtonAction;
-//  KodSetButton.Caption:='Set code';
-//end;
-//
-//
-//procedure TDAC_Show.Free;
-//begin
-// fKodShow.Free;
-// fValueShow.Free;
-//end;
-//
-//procedure TDAC_Show.KodSetButtonAction(Sender: TObject);
-//begin
-//   fOutputInterface.OutputInt(fKodShow.Data);
-//   fKodShow.ColorToActive(true);
-//   fValueShow.ColorToActive(false);
-//end;
-//
-//procedure TDAC_Show.ReadFromIniFile(ConfigFile: TIniFile);
-//begin
-//   fValueShow.ReadFromIniFile(ConfigFile);
-//   fKodShow.ReadFromIniFile(ConfigFile);
-//end;
-//
-//procedure TDAC_Show.ResetButtonClick(Sender: TObject);
-//begin
-// fOutputInterface.Reset();
-// fKodShow.ColorToActive(false);
-// fValueShow.ColorToActive(false);
-//end;
-//
-//
-//procedure TDAC_Show.ValueSetButtonAction(Sender: TObject);
-//begin
-// fOutputInterface.Output(fValueShow.Data);
-// fValueShow.ColorToActive(true);
-// fKodShow.ColorToActive(false);
-//end;
-//
-//procedure TDAC_Show.WriteToIniFile(ConfigFile: TIniFile);
-//begin
-//   fValueShow.WriteToIniFile(ConfigFile);
-//   fKodShow.WriteToIniFile(ConfigFile);
-//end;
-//
-//
-//{ TArduinoDACShow }
-//
-//constructor TArduinoDACShow.Create(ArdDAC: TArduinoDAC;
-//                                  PinLs: array of TPanel;
-//                                  PinVariant:TStringList;
-//                                  VData, KData: TStaticText;
-//                                  VL, KL: TLabel;
-//                                  VSB, KSB, RB: TButton);
-//begin
-// inherited Create(ArdDAC,PinLs,PinVariant);
-// fDAC_Show:=TDAC_Show.Create(ArdDAC,VData, KData,VL, KL, VSB, KSB, RB);
-//end;
-//
-//procedure TArduinoDACShow.Free;
-//begin
-// fDAC_Show.Free;
-// inherited Free;
-//end;
-//
-//procedure TArduinoDACShow.ReadFromIniFile(ConfigFile: TIniFile);
-//begin
-//  inherited ReadFromIniFile(ConfigFile);
-//  fDAC_Show.ReadFromIniFile(ConfigFile);
-//end;
-//
-//procedure TArduinoDACShow.WriteToIniFile(ConfigFile: TIniFile);
-//begin
-//  inherited WriteToIniFile(ConfigFile);
-//  fDAC_Show.WriteToIniFile(ConfigFile);
-//end;
-//
-//
+
 { TStringParameterShow }
 
 constructor TStringParameterShow.Create(STD: TStaticText;
